2796014511f0fe4cebcab849f58127f45ea14852
overtrue <anzhengchao@gmail.com>
2025-08-07 14:05:26 +0800

    wip

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,18 @@
 // See: https://rollupjs.org/introduction/
 
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
 const config = {
   input: 'src/index.ts',
   output: {
-    file: 'dist/index.cjs',
-    format: 'cjs',
-    sourcemap: true,
-    exports: 'auto'
+    esModule: true,
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
   },
-  plugins: [
-    typescript(),
-    nodeResolve({ preferBuiltins: true }),
-    commonjs(),
-    json()
-  ]
+  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
 }
 
 export default config
